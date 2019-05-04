cf187a14b1447f06458125c5c0e36555846c8486
drumath <2237k.drumath@gmail.com>
2019-05-05 02:16:33 +0900

    ログイン機能完全に実装した

---
+++ Assets/Scripts/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRoidSDK;
+using UnityEngine.SceneManagement;
+
+public class LoginController : MonoBehaviour
+{
+    [SerializeField] SDKConfiguration sdkConfiguration;
+    BrowserAuthorize browserAuthorize;
+
+    [SerializeField] Text errMessageBox;
+    public void Login()
+    {
+        Authentication.Instance.Init(sdkConfiguration.AuthenticateMetaData);
+        Authentication.Instance.AuthorizeWithExistAccount((bool isAuthSuccess) =>
+        {
+            if (!isAuthSuccess)
+            {
+                browserAuthorize = BrowserAuthorize.GenerateInstance(sdkConfiguration);
+                browserAuthorize.OpenBrowser(AfterAuth);
+            }
+            else
+            {
+                AfterAuth(true);
+            }
+        },
+        (System.Exception error) => {
+            errMessageBox.text = "error:\n" + error.ToString();
+        });
+    }
+
+    void AfterAuth(bool isRegistered)
+    {
+        SceneManager.LoadScene("Home");
+    }
+
+}
