5545837104e73e766bb199f316870354880c1e33
drumath <2237k.drumath@gmail.com>
2019-05-05 19:51:29 +0900

    ホーム画面のUIを大まかに配置してログイン中のユーザ情報も取得できた

---
+++ Assets/LoginUser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRoidSDK;
+
+public class LoginUser : MonoBehaviour
+{
+    void Update()
+    {
+        string t = "new text";
+        HubApi.GetAccount(
+            (Account acount) =>
+            {
+                t = acount.user_detail.user.name + "としてログイン中";
+                GetComponent<Text>().text = t;
+            },
+            (ApiErrorFormat error) => {
+                t = error.message;
+                GetComponent<Text>().text = t;
+            }
+        );
+
+        
+    }
+}
