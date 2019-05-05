e295cb308cecb43c0d97c7edf48cea3d450b2bea
drumath <2237k.drumath@gmail.com>
2019-05-05 20:36:51 +0900

    ログアウト機能を実装(キャッシュ破棄されてなくね)

---
+++ Assets/Scripts/LogoutButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRoidSDK;
+using UnityEngine.SceneManagement;
+
+public class LogoutButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        Authentication.Instance.Logout();
+        SceneManager.LoadScene("Login");
+    }
+}
