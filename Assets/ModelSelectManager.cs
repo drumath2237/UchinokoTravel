8ac4772ec9f3d989ff6d4ad595bf2c355890a943
drumath <2237k.drumath@gmail.com>
2019-05-09 11:53:50 +0900

    これからモデルの情報の取得→リスト表示に踏み込む！

---
+++ Assets/ModelSelectManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModelSelectManager : MonoBehaviour
+{
+    [SerializeField] GameObject content;
+    [SerializeField] GameObject characterModelPanel;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
