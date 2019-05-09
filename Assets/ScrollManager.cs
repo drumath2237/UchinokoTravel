0ecc9fbe47843cd7743bf10a586b14e4873056b9
drumath <2237k.drumath@gmail.com>
2019-05-09 11:07:22 +0900

    scriptからscrollviewにコンテンツを追加することに成功

---
+++ Assets/ScrollManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollManager : MonoBehaviour
+{
+    [SerializeField] GameObject scroll_text;
+    [SerializeField] GameObject scroll_content;
+    void Start()
+    {
+        for(int i=0; i<10; i++)
+        {
+            var txt = Instantiate(scroll_text, scroll_content.transform);
+            txt.GetComponent<UnityEngine.UI.Text>().text = i.ToString();
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
